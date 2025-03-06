8027a732aded329e04a1e310263563d5aa06685c
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-03-06 09:29:24 +0000

    Initial Commit

---
+++ Rectangle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Rectangle
+//
+//  Created by Sam Hart on 06/03/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
